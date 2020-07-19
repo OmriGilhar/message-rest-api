24404055fcd2dc8440b60373c65a541bbf3e5886
Omri Gilhar <omrig3@mail.afeka.ac.il>
2020-07-19 09:42:33 +0300

    Fixed predefiend db schema

--- db/__init__.py
+++ db/__init__.py
@@ -15,13 +15,14 @@ def init_db():
             sql = f.read()  # watch out for built-in `str`
             db.executescript(sql)
     except sqlite3.OperationalError as oe:
-        logging.error("Could not found .sql schema, create db with "
-                      "predefined sql schema\nError trace : {0}".format(
-                       str(oe)))
-        db.execute("""CREATE TABLE users( user_id INTEGER PRIMARY KEY,  
-        user_name text, password text); CREATE TABLE message( id INTEGER 
-        PRIMARY KEY, sender text, receiver text, message text, subject 
-        text, date date, unread INTEGER);""")
+        if 'already exists' not in str(oe):
+            logging.error("Could not found .sql schema, create db with "
+                          "predefined sql schema\nError trace : {0}".
+                          format(str(oe)))
+            db.execute("""CREATE TABLE users( user_id INTEGER PRIMARY KEY,  
+            user_name text, password text); CREATE TABLE message( id INTEGER 
+            PRIMARY KEY, sender text, receiver text, message text, subject 
+            text, date date, unread INTEGER);""")
     db.commit()
 
 
